e25fb905a96eb5e733b5969333fd483512b5e177
Khue Nguyen <Z5483Y@gmail.com>
2021-11-03 21:19:54 -0400

    Add missing update command to update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 
 test -f ./builder/cbase16.cpp || git clone https://github.com/base16-fork/cbase16 ./builder
 make -C ./builder
+./builder/cbase16 -l -c ./
 
 for f in $(find ./schemes -maxdepth 2 -type f -name "*.yaml"); do 
 	if [ ! -d "$(dirname "$(echo $f | sed "s/\.\/schemes/\./")")" ]; then
